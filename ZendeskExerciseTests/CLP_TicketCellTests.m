a38ebae1be83337908680b3ef515d5dc8893e56d
dacioleal <dacioleal@gmail.com>
2016-10-30 21:07:48 +0100

    Test for TicketCell

--- ZendeskExerciseTests/CLP_TicketCellTests.m
+++ ZendeskExerciseTests/CLP_TicketCellTests.m
@@ -8,12 +8,12 @@
 
 #import <XCTest/XCTest.h>
 #import "CLP_TicketCell.h"
-#import "CLP_Ticket.h"
 
-@interface CLP_TicketCellTests : XCTestCase <UITableViewDataSource>
+
+@interface CLP_TicketCellTests : XCTestCase
 
 @property (strong, nonatomic) CLP_TicketCell *sut;
-@property (strong, nonatomic) UITableView *fakeTableView;
+
 
 @end
 
@@ -22,9 +22,7 @@
 - (void)setUp {
     [super setUp];
     
-    self.fakeTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 300.0, 300.0) style:UITableViewStylePlain];
-    self.fakeTableView.dataSource = self;
-    [self.fakeTableView registerClass:[CLP_TicketCell class] forCellReuseIdentifier:@"TicketCell"];
+    self.sut = [[CLP_TicketCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"TicketCell"];
 }
 
 - (void)tearDown {
@@ -33,25 +31,9 @@
 }
 
 - (void)testConfigTicketCell_SetsTextLabelAndDetailTextLabel {
-    
-    self.sut = [self.fakeTableView dequeueReusableCellWithIdentifier:@"TicketCell" forIndexPath:[NSIndexPath indexPathForItem:0 inSection:0]];
     [self.sut configureCellWithTitle:@"Test title" andDescription:@"Test description"];
-    NSLog(@"Detail: %@", self.sut.detailTextLabel.text);
     XCTAssertEqual(self.sut.textLabel.text, @"Test title");
     XCTAssertEqual(self.sut.detailTextLabel.text, @"Test description");
 }
-
-
-
-
-#pragma mark - Fake TableView datasource
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return [tableView dequeueReusableCellWithIdentifier:@"TicketCell" forIndexPath:indexPath];
-}
-
 
 @end
